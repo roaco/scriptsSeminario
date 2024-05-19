cca82a5c89a7ff0f17338dd045ba7124eee2f5e8
Rodrigo Acosta Restrepo <roaco_91@hotmail.com>
2024-05-18 19:52:32 -0500

    Se realizan pruebas del script y se confirma su funcionalidad.

--- backup_DB.sh
+++ backup_DB.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Intentamos crear el directorio de backups. Si ya existe, no hacemos nada.
-mkdir -p "$BACKUP_DIR" || echo "Directorio de backups ya existe: $BACKUP_DIR"
 #Contarseña usuario root
-MYSQL_PASSWORD="*****"
+MYSQL_PASSWORD="****"
 DATE=$(date +%d-%m-%Y)
 #Ruta donde se guardara nuestro backup
 BACKUP_DIR="/backup/db-backup"
-#Nombre de la BD a realziar backup
-DB=testdb
+# Intentamos crear el directorio de backups. Si ya existe, no hacemos nada.
+mkdir -p "$BACKUP_DIR" || echo "Directorio de backups ya existe: $BACKUP_DIR"
+#Nombre de la BD a realizar backup
+DB=test_database
 echo $DB
 mysqldump -u root -p$MYSQL_PASSWORD $DB | gzip -9 > "$BACKUP_DIR/testdb-$DATE.sql.gz"
 find $BACKUP_DIR/* -mtime +5 -exec rm {} \;
